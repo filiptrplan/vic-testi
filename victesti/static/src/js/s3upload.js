e9fca7b786fb365a1bed8798e3cc1b621f5662d4
Filip Trplan <tg.trplan@gmail.com>
2020-11-23 01:06:25 +0100

    upload the file with the sha256 hash as name

--- victesti/static/src/js/s3upload.js
+++ victesti/static/src/js/s3upload.js
@@ -1,3 +1,6 @@
+import "js-sha256";
+import { sha256 } from "js-sha256";
+
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== "") {
@@ -21,23 +24,32 @@ const csrftoken = getCookie("csrftoken");
  * Retrieve pre-signed POST data from a dedicated API endpoint.
  * @returns {Promise<any>}
  */
-const getPresignedPostData = (fileName) => {
+const getPresignedPostData = (file) => {
     return new Promise((resolve) => {
         const xhr = new XMLHttpRequest();
 
-        // Set the proper URL here.
+        // Set the proper URL here. Is set on the HTML template
         const url = s3signatureURL;
 
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader(
-            "Content-Type",
-            "application/x-www-form-urlencoded"
-        );
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        xhr.send(`file_name=${fileName}`);
-        xhr.onload = function () {
-            resolve(JSON.parse(this.responseText));
-        };
+        const fileReader = new FileReader();
+        fileReader.readAsArrayBuffer(file);
+        fileReader.onload = () => {
+            let arrayBuffer = fileReader.result;
+            let fileHash = sha256(arrayBuffer);
+            let fileName = fileHash + '.' + file.name.split(".").pop();
+            console.log(fileName);
+            
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader(
+                "Content-Type",
+                "application/x-www-form-urlencoded"
+            );
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            xhr.send(`file_name=${fileName}`);
+            xhr.onload = function () {
+                resolve(JSON.parse(this.responseText));
+            };
+        }
     });
 };
 /**
@@ -66,7 +78,7 @@ const uploadFileToS3 = (presignedPostData, file, updateProgressBar) => {
 
 export default function uploadFile(file, updateProgressBar) {
     return new Promise((resolve, reject) => {
-        getPresignedPostData(file.name).then((presignedPostData) => {
+        getPresignedPostData(file).then((presignedPostData) => {
             uploadFileToS3(presignedPostData, file, updateProgressBar)
                 .then(() => {
                     resolve();
