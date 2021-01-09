89113b694ba5bf1a575967748231ae7f8161c05d
Filip Trplan <tg.trplan@gmail.com>
2021-01-09 17:54:24 +0100

    clean up js code

--- victesti/static/src/js/base.js
+++ victesti/static/src/js/base.js
@@ -1,24 +1,6 @@
 import $ from "cash-dom";
-import { setCookie, getCookie, eraseCookie } from "./cookies";
-import BulmaNotification from './bulma-notification';
-import {ajax} from './ajax'
-
-let facebookConnectedInit = false;
-let facebookInitCheck = false;
-
-if(getCookie('FBConnected') == null){
-    setCookie('FBConnected', '0');
-}
-
-if(getCookie('FBConnected') == 0) {
-    facebookConnectedInit = false;
-    $("#facebookLoginButton").html("PRIJAVA S FACEBOOKOM");
-    $("#facebookName").html("");
-} else if (getCookie('FBConnected') == 1){
-    facebookConnectedInit = true;
-    $("#facebookLoginButton").html("ODJAVA");
-    $("#facebookName").html(`(${getCookie('FBName')})`);
-}
+import {ajax} from './ajax';
+import getCookie from './cookies';
 
 $(document).ready(function () {
     // Check for click events on the navbar burger icon
@@ -26,14 +8,6 @@ $(document).ready(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-    });
-
-    $('#facebookLoginButton').on('click', () => {
-        if(getCookie('FBConnected') == 1) {
-            FB.api('/me/permissions', 'delete', handleConnected);
-        } else {
-            FB.login(handleConnected);
-        }
     });
 });
 
@@ -45,35 +19,21 @@ window.fbAsyncInit = function () {
         cookie: true,
         version: "v9.0",
     });
-    FB.getLoginStatus(handleConnected);
 };
+
+function login() {
+    FB.login(handleConnected);
+}
+
+function logout() {
+    FB.api("/me/permissions", "delete", handleConnected);
+    ajax("POST", "/api/logout", [], getCookie('csrftoken'), 'json')
+}
 
 function handleConnected(response){
     if (response.status == "connected") {
-        $('#facebookLoginButton').html('ODJAVA');
         ajax("POST", "/api/login", {
             fb_token: response.authResponse.accessToken,
         }, getCookie('csrftoken'), 'json');
-        FB.api('/me', (response) => {
-            $('#facebookName').html(`(${response.name})`);
-            setCookie('FBName', response.name);
-        });
-        setCookie('FBConnected', '1');
-        setCookie('FBAccessToken', response.authResponse.accessToken);
-    } else {
-        if(facebookConnectedInit && !facebookInitCheck) {
-            const warning = new BulmaNotification(`
-            Za pravilno delovanje strani prosim dovolite VSE piškotke.
-            `, '.content', {
-                prepend: true,
-                type: 'warning'
-            });
-        } else {
-            $('#facebookLoginButton').html('PRIJAVA S FACEBOOKOM');
-            $('#facebookName').html('');
-            setCookie('FBConnected', '0');
-            eraseCookie('FBAccessToken');
-        }
-        facebookInitCheck = true;
     }
 }
