97f5e5658fecc02e4b2177022980df95745901f1
Filip Trplan <tg.trplan@gmail.com>
2021-01-09 15:46:41 +0100

    switch debug with env file

--- victesti/victesti/settings.py
+++ victesti/victesti/settings.py
@@ -30,7 +30,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 if settings.DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
