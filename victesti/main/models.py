94bed0e6842783e78b81b9eb00f862d68a2de3ae
Filip Trplan <filip@trplan.si>
2022-06-03 13:39:34 +0200

    add privacy policy

--- victesti/main/models.py
+++ victesti/main/models.py
@@ -31,19 +31,13 @@ class User(AbstractBaseUser):
 
     @property
     def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
         return self.is_admin
 
     def __str__(self):
         return self.name
 
     def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
         return True
 
     def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
         return True
