ab0f09f14e7bfee0283ce05eb255114b33886fcc
Filip Trplan <filip@trplan.si>
2022-06-05 13:52:50 +0200

    privacy policy

--- victesti/main/urls.py
+++ victesti/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="main/index.html"), name='main.home'),
+    path('privacy', TemplateView.as_view(template_name="main/privacy.html"), name='main.privacy'),
     path('api/login', views.api_login, name='main.login'),
     path('api/logout', views.api_logout, name='main.logout')
 ]
